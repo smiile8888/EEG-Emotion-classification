bd713ed6550c5b234d54e778c2d7d69227071376
smiile8888 <th.thanyathorn@gmail.com>
2019-10-31 00:07:34 -0400

    Modify code for Python 3

--- creating_vector.py
+++ creating_vector.py
@@ -6,6 +6,7 @@ from numpy.fft import *
 from scipy import *
 from pylab import *
 import pywt
+
 fout_data = open("train.csv",'a')
 vec = []
 chan = ['Fp1','AF3','F3','F7','FC5','FC1','C3','T7','CP5','CP1','P3','P7','PO3','O1','Oz','Pz','Fp2','AF4','Fz','F4','F8','FC6','FC2','Cz','C4','T8','CP6','CP2','P4','P8','PO4','O2']
@@ -18,7 +19,7 @@ with open("features_raw.csv") as f:
             columns[k].append(v) # append the value into the appropriate list
                                  # based on column name k
 for i in chan:
-	x = np.array(columns[i]).astype(np.float)
+    x = np.array(columns[i]).astype(np.float)
 	coeffs = pywt.wavedec(x, 'db4', level=6)
 	cA6, cD6, cD5,cD4,cD3,cD2,cD1 = coeffs
 	cD5 = np.std(cD5)
@@ -38,5 +39,6 @@ for i in chan:
 		fout_data.write(str(cD3)+",")
 		fout_data.write(str(cD2)+",")
 		fout_data.write(str(cD1)+",")
+
 fout_data.write("\n")
 fout_data.close()
--- svm_clas.py
+++ svm_clas.py
@@ -1,5 +1,6 @@
 from sklearn import svm
 import numpy as np
+
 train_y = []
 train_a = []
 train_x = np.genfromtxt('train.csv',delimiter=',')
@@ -9,9 +10,9 @@ for i in f:
 train_y = np.array(train_y).astype(np.float)
 train_y = train_y.astype(np.int)
 train_x = np.array(train_x)
-#print "valence",train_y
-#print train_x
-#print "train_x",train_x
+# print "valence",train_y
+# print train_x
+# print "train_x",train_x
 clf = svm.SVC()
 clf.fit(train_x, train_y)
 
@@ -21,23 +22,23 @@ for i in f:
 	train_a.append(i)
 train_a = np.array(train_a).astype(np.float)
 train_a = train_a.astype(np.int)
-#print "arousal",train_a[1040:1280]
-#print "train_x",len(train_x[0:26])
+# print "arousal",train_a[1040:1280]
+# print "train_x",len(train_x[0:26])
 clf1 = svm.SVC()
 clf1.fit(train_x, train_a)
-#print test_a
+# print test_a
 predict_al = clf1.predict(train_x)
-#print "alrosal",predict_al
+# print "alrosal",predict_al
 predict_val = clf.predict(train_x) 
-#print "valence",predict_val 
+# print "valence",predict_val
 val_count = al_count = 0
 for i in range(len(train_y)):
 	if train_y[i] == predict_val[i]:
 		val_count = val_count+1
 	if train_a[i] == predict_al[i]:
 		al_count = al_count+1
-print "predicted valence",(float(val_count)/len(train_y))*100
-print "predicted arousal",(float(al_count)/len(train_y))*100
+print("predicted valence",(float(val_count)/len(train_y))*100)
+print("predicted arousal",(float(al_count)/len(train_y))*100)
 # classifier efficiency
 '''
 predicted valence 98.046875 percentage
@@ -51,5 +52,5 @@ predicted arousal 96.09375
 predicted valence 17.9166666667
 predicted arousal 13.3333333333
 '''
-#chan = ['Fp1','AF3','F3','F7','FC5','FC1','C3','T7','CP5','CP1','P3','P7','PO3','O1','Oz','Pz','Fp2','AF4','Fz','F4','F8','FC6','FC2','Cz','C4','T8','CP6','CP2','P4','P8','PO4','O2']
+# chan = ['Fp1','AF3','F3','F7','FC5','FC1','C3','T7','CP5','CP1','P3','P7','PO3','O1','Oz','Pz','Fp2','AF4','Fz','F4','F8','FC6','FC2','Cz','C4','T8','CP6','CP2','P4','P8','PO4','O2']
 
